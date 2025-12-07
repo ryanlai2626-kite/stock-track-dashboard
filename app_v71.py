0ddd726b0484f4a846cf6bed156547e281258809
ryanlai2626-kite <ryanlai2626@gmail.com>
2025-12-08 07:24:39 +0800

    Update app_v71.py

--- app_v71.py
+++ app_v71.py
@@ -23,7 +23,7 @@ st.markdown("""
 <style>
     /* 1. 全域背景 (淺灰藍) 與深色文字 */
     .stApp {
-        background-color: #FFFFFF !important;
+        background-color: #e8e8e8 !important;
         color: #333333 !important;
         font-family: 'Helvetica', 'Arial', sans-serif;
     }
@@ -635,6 +635,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
